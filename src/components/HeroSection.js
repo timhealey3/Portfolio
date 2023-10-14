6bfd84f668d2808b7b34f4e75c4963e9cbba7625
timhealey3 <timhealey34@gmail.com>
2023-10-14 15:49:30 -0400

    typed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,9 +2,27 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import {TypeAnimation}
 
 function HeroSection() {
   return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'We produce food for Mice',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'We produce food for Hamsters',
+        1000,
+        'We produce food for Guinea Pigs',
+        1000,
+        'We produce food for Chinchillas',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
     <div className='hero-container'>
       <video src='/videos/numberBackground.mp4' autoPlay loop muted />
       <h1>Hi, I'm Tim Healey</h1>
