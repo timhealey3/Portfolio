c105950c270aa13adff2642208a5937b4892a78c
timhealey3 <timhealey34@gmail.com>
2023-10-13 21:25:51 -0400

    removed type

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../App.css';
-//import Typed from 'react-typed';
 import { Button } from './Button';
 import './HeroSection.css';
 
@@ -9,14 +8,6 @@ function HeroSection() {
     <div className='hero-container'>
       <video src='/videos/numberBackground.mp4' autoPlay loop muted />
       <h1>Hi, I'm Tim Healey</h1>
-      <p>
-    /*  <Typed
-        strings={["I am currently looking for my first job!", "I am a Full Stack Developer", "I am a Senior at Western Michigan University"]}
-        typeSpeed={150}
-        backSpeed={100}
-        loop
-      /> */
-        </p>
         <div className='hero-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
         Resume
