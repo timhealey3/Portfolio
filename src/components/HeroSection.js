6741db00999d5a84c17a4fa5cd4fe06c83106e25
timhealey3 <timhealey34@gmail.com>
2023-10-13 21:22:07 -0400

    sdasda:

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import Typed from 'react-typed';
+//import Typed from 'react-typed';
 import { Button } from './Button';
 import './HeroSection.css';
 
@@ -10,12 +10,12 @@ function HeroSection() {
       <video src='/videos/numberBackground.mp4' autoPlay loop muted />
       <h1>Hi, I'm Tim Healey</h1>
       <p>
-      <Typed
+    /*  <Typed
         strings={["I am currently looking for my first job!", "I am a Full Stack Developer", "I am a Senior at Western Michigan University"]}
         typeSpeed={150}
         backSpeed={100}
         loop
-      />
+      /> */
         </p>
         <div className='hero-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
