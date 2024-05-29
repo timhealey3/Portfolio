9b8e11fa70cace5d4d899f277bf572afd9201d9d
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2024-05-28 20:59:46 -0400

    Update Exper.js

--- src/components/Exper.js
+++ src/components/Exper.js
@@ -5,18 +5,19 @@ const Exper = () => {
   return (
     <section id='exper'>
       <span className='skillTitle'>About Me</span>
-      <span className='skillDesc'>I am currently looking for a software engineering job</span>
+      <span className='skillDesc'>I am currently working on a temporary contract for Brooksource. Where I work at Auto Owners Insurance as a Java dev!</span>
       <div className="skillBar">
         <img src='images/wmu.png' alt='Education' className='skillBarImg' />
         <div className="skillBarText">
           <h2>Education</h2>
-          <p>BS in Computer Science, 3.57 GPA, 2019-2024</p>
+          <p>BS in Computer Science, 3.60 GPA, 2019-2024</p>
         </div>
       </div>
       <div className="skillBar">
         <img src='images/tgw.png' alt='Work' className='skillBarImg' />
         <div className="skillBarText">
           <h2>Work Experience</h2>
+          <p>Auto-Owners Insurance [Contracted by Brooksource]: Associate Software Developer/p>
           <p>TGW North America: Software Engineer Intern</p>
         </div>
       </div>
