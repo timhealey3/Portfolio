0ed762b96a4ec586a3f9a4198827539175b011b7
timhealey3 <timhealey34@gmail.com>
2024-06-02 21:45:11 -0400

    update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
+import React from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
 
 function Cards() {
   return (
-    <div className='cards'>
+    <div className="cards">
       <h1>Check out some of my personal projects!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
             <CardItem
-              src='images/sfml.png'
-              text='Roguelike RPG'
-              label='C++'
-              path='https://github.com/timhealey3/RPG-Survival'
+              src="images/gameScreenshot.png"
+              text="Roguelike RPG"
+              label="C++"
+              path="https://github.com/timhealey3/RPG-Survival"
             />
             <CardItem
-              src='images/statApp.png'
-              text='Goalie Stat Tracker'
-              label='React Native'
-              path='https://play.google.com/store/apps/details?id=com.timhealey34.GoalieStats'
+              src="images/statApp.png"
+              text="Goalie Stat Tracker"
+              label="React Native"
+              path="https://play.google.com/store/apps/details?id=com.timhealey34.GoalieStats"
             />
           </ul>
-          <ul className='cards__items'>
+          <ul className="cards__items">
             <CardItem
-              src='images/webImage.png'
-              text='This Website!'
-              label='React'
-              path='https://github.com/timhealey3/ResumeWebSite'
+              src="images/webImage.png"
+              text="This Website!"
+              label="React"
+              path="https://github.com/timhealey3/ResumeWebSite"
             />
             <CardItem
-              src='images/popularMovies.png'
-              text='Popular Movies Now'
-              label='Python'
-              path='https://github.com/timhealey3/popular_movies_now'
+              src="images/popularMovies.png"
+              text="Popular Movies Now"
+              label="Python"
+              path="https://github.com/timhealey3/popular_movies_now"
             />
             <CardItem
-              src='images/passwordGen.png'
-              text='Random Password Generator'
-              label='Python'
-              path='https://github.com/timhealey3/Password-Generator'
+              src="images/passwordGen.png"
+              text="Random Password Generator"
+              label="Python"
+              path="https://github.com/timhealey3/Password-Generator"
             />
           </ul>
         </div>
--- src/components/Exper.js
+++ src/components/Exper.js
@@ -1,35 +1,42 @@
-import React from 'react';
-import './Exper.css';
+import React from "react";
+import "./Exper.css";
 
 const Exper = () => {
   return (
-    <section id='exper'>
-      <span className='skillTitle'>About Me</span>
-      <span className='skillDesc'>I am currently working on a temporary contract for Brooksource. Where I work at Auto Owners Insurance as a Java dev!</span>
+    <section id="exper">
+      <span className="skillTitle">About Me</span>
+      <span className="skillDesc">
+        I am currently working on a temporary contract for Brooksource. Where I
+        work at Auto Owners Insurance as a Java dev. I spend my free time
+        working on games and learning about machine learning!
+      </span>
       <div className="skillBar">
-        <img src='images/wmu.png' alt='Education' className='skillBarImg' />
+        <img src="images/wmu.png" alt="Education" className="skillBarImg" />
         <div className="skillBarText">
           <h2>Education</h2>
           <p>BS in Computer Science, 3.60 GPA, 2019-2024</p>
         </div>
       </div>
       <div className="skillBar">
-        <img src='images/tgw.png' alt='Work' className='skillBarImg' />
+        <img src="images/tgw.png" alt="Work" className="skillBarImg" />
         <div className="skillBarText">
           <h2>Work Experience</h2>
-          <p>Auto-Owners Insurance [Contracted by Brooksource]: Associate Software Developer</p>
+          <p>
+            Auto-Owners Insurance [Contracted by Brooksource]: Associate
+            Software Dev
+          </p>
           <p>TGW North America: Software Engineer Intern</p>
         </div>
       </div>
       <div className="skillBar">
-        <img src='images/c++.png' alt='Skills' className='skillBarImg' />
+        <img src="images/c++.png" alt="Skills" className="skillBarImg" />
         <div className="skillBarText">
           <h2>Skills</h2>
           <p>C++, C, Java, Python, Javascript</p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Exper
+export default Exper;
